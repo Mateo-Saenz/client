850f2c3777a3e3a7cac60802684194b865ca34fe
Mate Piola <mateosaenz107@gmail.com>
2024-03-01 20:31:19 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+import Thumbnail1 from './img/blog1.jpg'
+import Thumbnail2 from './img/blog2.jpg'
+import Thumbnail3 from './img/blog3.jpg'
+import Thumbnail4 from './img/blog4.jpg'
+
+export const DUMMY_POSTS = [
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'education',
+        title: 'Fisrt post',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate consequatur fuga voluptatem in eligendi ab, laboriosam dicta sunt repellat ipsum.',
+        authorID: 3
+    },
+    {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'science',
+        title: 'Second post',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate consequatur fuga voluptatem in eligendi ab, laboriosam dicta sunt repellat ipsum.',
+        authorID: 7
+    },
+    {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'weather',
+        title: 'FThird post',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate consequatur fuga voluptatem in eligendi ab, laboriosam dicta sunt repellat ipsum.',
+        authorID: 43
+    },
+    {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'farming',
+        title: 'Fourth post',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate consequatur fuga voluptatem in eligendi ab, laboriosam dicta sunt repellat ipsum.',
+        authorID: 32
+    },
+    
+]
